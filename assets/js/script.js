b6d3e70b23d979ade319d55672a733ef8cec1611
Nur Muhammad Himawan <muhammad.himawan73@gmail.com>
2022-11-16 11:27:23 +0700

    add fresh-rotten page

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,33 @@ window.addEventListener("scroll", function () {
         navbar.classList.remove("navbar-anim");
     }
 })
+
+
+// Show upload image
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#imageResult').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+$(function () {
+    $('#upload').on('change', function () {
+        readURL(input);
+    })
+})
+
+// Show upload image name
+const input = document.getElementById('upload');
+const infoArea = document.getElementById('upload-label');
+
+input.addEventListener('change', showFileName);
+function showFileName(e) {
+    const input = e.srcElement;
+    const fileName = input.files[0].name;
+    infoArea.textContent = 'File: ' + fileName;
+}
