7236b889cb083b74c0efafd621d9cdd6b72f8018
Nur Muhammad Himawan <muhammad.himawan73@gmail.com>
2022-11-21 08:30:45 +0700

    fix styling and delete unused files

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,12 @@ const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function () {
     if (this.window.scrollY >= 150) {
+        document.getElementById("navbar").style.transition = "none";
         navbar.classList.add("active");
         backTopBtn.classList.add("active");
         navbar.classList.add("navbar-anim");
     } else {
+        document.getElementById("navbar").style.transition = "all 1s";
         navbar.classList.remove("active");
         backTopBtn.classList.remove("active");
         navbar.classList.remove("navbar-anim");
